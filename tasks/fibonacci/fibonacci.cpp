cddba1e3949f1e16d872b83010de0fcdfe1aa6ef
= <=>
2023-04-13 18:13:25 +0300

    Finished

--- tasks/fibonacci/fibonacci.cpp
+++ tasks/fibonacci/fibonacci.cpp
@@ -3,11 +3,9 @@
 int64_t CalculateNthFibonacci(int64_t n) {
     if (n == 0) {
         return 0;
-    }
-    else if (n == 1){
+    } else if (n == 1) {
         return 1;
-    }
-    else {
+    } else {
         return CalculateNthFibonacci(n - 1) + CalculateNthFibonacci(n - 2);
     }
 }
