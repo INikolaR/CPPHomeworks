ac0aa4fcde9c1223339d1314398e3722b913f229
= <=>
2023-04-13 17:48:00 +0300

    Finished

--- tasks/fibonacci/fibonacci.cpp
+++ tasks/fibonacci/fibonacci.cpp
@@ -1,5 +1,13 @@
 #include "fibonacci.h"
 
 int64_t CalculateNthFibonacci(int64_t n) {
-    return 0;
+    if (n == 0) {
+        return 0;
+    }
+    else if (n == 1){
+        return 1;
+    }
+    else {
+        return CalculateNthFibonacci(n - 1) + CalculateNthFibonacci(n - 2);
+    }
 }
