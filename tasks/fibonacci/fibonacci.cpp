f257285e3888b672964e8727e980b6de84744933
= <=>
2023-04-13 18:21:49 +0300

    Finished

--- tasks/fibonacci/fibonacci.cpp
+++ tasks/fibonacci/fibonacci.cpp
@@ -3,9 +3,16 @@
 int64_t CalculateNthFibonacci(int64_t n) {
     if (n == 0) {
         return 0;
-    } else if (n == 1) {
-        return 1;
-    } else {
-        return CalculateNthFibonacci(n - 1) + CalculateNthFibonacci(n - 2);
     }
+    if (n == 1) {
+        return 1;
+    }
+    int64_t a = 0;
+    int64_t b = 1;
+    for (int i = 1; i < n; ++i) {
+        int64_t t = a + b;
+        a = b;
+        b = t;
+    }
+    return b;
 }
