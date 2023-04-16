cea594ca82a211c1d0c0a7fead394e30f3caa7f8
= <=>
2023-04-16 13:53:34 +0300

    Finished

--- tasks/sum_digits/sum_digits.cpp
+++ tasks/sum_digits/sum_digits.cpp
@@ -2,5 +2,11 @@
 
 
 int64_t SumOfDigits(int64_t number) {
-    return 0;
+    const int64_t p = 10;
+    int64_t ans = 0;
+    while (number > 0) {
+        ans += number % p;
+        number /= p;
+    }
+    return ans;
 }
