f38867b34a2fb8eb06dc9739ed998d6a5401ddd3
= <=>
2023-04-16 13:49:34 +0300

    Finished

--- tasks/reduce_fraction/reduce_fraction.cpp
+++ tasks/reduce_fraction/reduce_fraction.cpp
@@ -1,6 +1,13 @@
 #include "reduce_fraction.h"
 
+#include <math.h>
+
+#include <numeric>
 
 int64_t ReduceFraction(int64_t numerator, int64_t denominator) {
-    return 1;
+    int64_t sign = numerator * denominator > 0 ? 1 : -1;
+    int64_t d = std::gcd(abs(numerator), abs(denominator));
+    int64_t a = abs(numerator) / d;
+    int64_t b = abs(denominator) / d;
+    return sign * a + b;
 }
