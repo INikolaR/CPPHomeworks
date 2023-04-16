46d77e3553f54520114fdd697989f802e24267dc
= <=>
2023-04-16 13:16:12 +0300

    Finished

--- tasks/is_leap/is_leap.cpp
+++ tasks/is_leap/is_leap.cpp
@@ -1,5 +1,7 @@
 #include "is_leap.h"
 
 bool IsLeap(int year) {
-    return true;
+    const int one_hundred = 100;
+    const int four_hundred = 400;
+    return year % four_hundred == 0 || (year % 4 == 0 && year % one_hundred != 0);
 }
