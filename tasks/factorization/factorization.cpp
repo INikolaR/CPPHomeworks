31caefaff25d2789823e78c81968675e5ed6565d
= <=>
2023-04-20 17:48:56 +0300

    Finished

--- tasks/factorization/factorization.cpp
+++ tasks/factorization/factorization.cpp
@@ -1,5 +1,16 @@
 #include "factorization.h"
 
+#include <cstdint>
+
 std::vector<std::pair<int64_t, int>> Factorize(int64_t x) {
-    return {};
+    std::vector<std::pair<int64_t, int>> ans = std::vector<std::pair<int64_t, int>>(0);
+    for (int64_t i = 2; i * i <= x; ++i) {
+        int num = 0;
+        while (x % i == 0) {
+            ++num;
+            x /= i;
+        }
+        ans.push_back({i, num});
+    }
+    return ans;
 }
