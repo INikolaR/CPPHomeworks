f4f7fc9415b9bfe3394517c37fff3fd0bd218929
= <=>
2023-04-20 17:34:37 +0300

    Finished

--- tasks/const_diagonal/const_diagonal.cpp
+++ tasks/const_diagonal/const_diagonal.cpp
@@ -1,4 +1,22 @@
 #include "const_diagonal.h"
 
 void BuildDiagonalArray(TableType table) {
+    const int side = 9;
+    int counter = 0;
+    int i = 0;
+    int j = 0;
+    table[i][j] = 0;
+    while (i != side || j != side) {
+        if (i == side) {
+            i = j + 1;
+            j = side;
+        } else if (j == 0) {
+            j = i + 1;
+            i = 0;
+        } else {
+            --j;
+            ++i;
+        }
+        table[i][j] = ++counter;
+    }
 }
