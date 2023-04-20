bdbe9981209484e37c25bdba6e4b33b76df8ae96
= <=>
2023-04-20 16:40:55 +0300

    Finished

--- tasks/palindrome/palindrome.cpp
+++ tasks/palindrome/palindrome.cpp
@@ -1,5 +1,21 @@
 #include "palindrome.h"
 
 bool IsPalindrome(const std::string& str) {
-    return false;
+    size_t l = 0;
+    size_t r = str.length() - 1;
+    while (l < r) {
+        while (str[l] == ' ') {
+            ++l;
+        }
+        while (str[r] == ' ') {
+            --r;
+        }
+        if (str[l] != str[r]) {
+            return false;
+        } else {
+            ++l;
+            --r;
+        }
+    }
+    return true;
 }
