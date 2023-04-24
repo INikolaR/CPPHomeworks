470d73e65c568456c77286671609728ac511b6d7
= <=>
2023-04-24 21:02:03 +0300

    Finished

--- tasks/range/range.cpp
+++ tasks/range/range.cpp
@@ -2,12 +2,21 @@
 
 std::vector<int> Range(int from, int to, int step) {
     std::vector<int> ans = std::vector<int>();
+    if (step == 0) {
+        return ans;
+    }
     if (step > 0) {
+        if (from >= to) {
+            return ans;
+        }
         while (from < to) {
             ans.push_back(from);
             from += step;
         }
     } else {
+        if (from <= to) {
+            return ans;
+        }
         while (from > to) {
             ans.push_back(from);
             from += step;
