b3db375088b6b4556928bad288131fe18af66b3c
= <=>
2023-04-24 20:22:01 +0300

    Finished

--- tasks/range/range.cpp
+++ tasks/range/range.cpp
@@ -2,9 +2,16 @@
 
 std::vector<int> Range(int from, int to, int step) {
     std::vector<int> ans = std::vector<int>();
-    while (from < to) {
-        ans.push_back(from);
-        from += step;
+    if (step > 0) {
+        while (from < to) {
+            ans.push_back(from);
+            from += step;
+        }
+    } else {
+        while (from > to) {
+            ans.push_back(from);
+            from += step;
+        }
     }
     return ans;
 }
