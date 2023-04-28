222d82e5ae56568ff95be928cf1b850ac4a01110
= <=>
2023-04-28 09:13:35 +0300

    Finished

--- tasks/sort_students/sort_students.cpp
+++ tasks/sort_students/sort_students.cpp
@@ -2,40 +2,20 @@
 
 #include <algorithm>
 
-bool operator==(Date date1, Date date2) {
-    return date1.year == date2.year && date1.month == date2.month && date1.day == date2.day;
+bool ComparatorByDate(Student a, Student b) {
+    return std::tie(a.birth_date.year, a.birth_date.month, a.birth_date.day, a.last_name, a.name) <
+           std::tie(b.birth_date.year, b.birth_date.month, b.birth_date.day, b.last_name, b.name);
 }
 
-bool operator<(Date date1, Date date2) {
-    if (date1.year == date2.year) {
-        if (date1.month == date2.month) {
-            return date1.day < date2.day;
-        }
-        return date1.month < date2.month;
-    }
-    return date1.year < date2.year;
+bool ComparatorByName(Student a, Student b) {
+    return std::tie(a.last_name, a.name, a.birth_date.year, a.birth_date.month, a.birth_date.day) <
+           std::tie(b.last_name, b.name, b.birth_date.year, b.birth_date.month, b.birth_date.day);
 }
 
 void SortStudents(std::vector<Student>& students, SortKind sort_kind) {
     if (sort_kind == SortKind::Date) {
-        std::sort(students.begin(), students.end(), [](Student a, Student b) {
-            if (a.birth_date == b.birth_date) {
-                if (a.last_name == b.last_name) {
-                    return a.name < b.name;
-                }
-                return a.last_name < b.last_name;
-            }
-            return a.birth_date < b.birth_date;
-        });
+        std::sort(students.begin(), students.end(), ComparatorByDate);
     } else {
-        std::sort(students.begin(), students.end(), [](Student a, Student b) {
-            if (a.last_name == b.last_name) {
-                if (a.name == b.name) {
-                    return a.birth_date < b.birth_date;
-                }
-                return a.name < b.name;
-            }
-            return a.last_name < b.last_name;
-        });
+        std::sort(students.begin(), students.end(), ComparatorByName);
     }
 }
