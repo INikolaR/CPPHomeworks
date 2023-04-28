63f2b89bfdcd0989907919f768c6c887f7e634c3
= <=>
2023-04-28 08:40:21 +0300

    Finished

--- tasks/sort/sort.h
+++ tasks/sort/sort.h
@@ -2,4 +2,11 @@
 
 template <class Iterator, class Comparator = std::less<typename std::iterator_traits<Iterator>::value_type>>
 void Sort(Iterator begin, Iterator end, Comparator comparator = Comparator()) {
+    for (auto it = begin; it < end; ++it) {
+        for (auto jt = begin; jt < end - 1; ++jt) {
+            if (!comparator(*jt, *(jt + 1))) {
+                std::swap(*jt, *(jt + 1));
+            }
+        }
+    }
 }
