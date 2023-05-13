1373c3234ba7a72df706170b5738724a0855fcc4
= <=>
2023-05-13 08:16:37 +0300

    Finished

--- tasks/is_sorted_until/is_sorted_until.h
+++ tasks/is_sorted_until/is_sorted_until.h
@@ -1,4 +1,11 @@
 #pragma once
 
 template <typename T>
-T IsSortedUntil(T begin, T end);
+T IsSortedUntil(T begin, T end) {
+    for (T i = begin + 1; i < end; ++i) {
+        if (*i < *(i - 1)) {
+            return i;
+        }
+    }
+    return end;
+}
