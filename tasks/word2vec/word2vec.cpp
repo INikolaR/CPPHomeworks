b895b12a3600468fffcfbd3e851ad2ab2d64a377
= <=>
2023-05-20 22:25:25 +0300

    Finshed

--- tasks/word2vec/word2vec.cpp
+++ tasks/word2vec/word2vec.cpp
@@ -2,7 +2,35 @@
 
 #include <vector>
 
+int64_t Scalar(const std::vector<int>& first_vector, const std::vector<int>& second_vector) {
+    int64_t sum = 0;
+    for (size_t i = 0; i < first_vector.size(); ++i) {
+        sum += first_vector[i] * second_vector[i];
+    }
+    return sum;
+}
+
 std::vector<std::string> FindClosestWords(const std::vector<std::string>& words,
                                           const std::vector<std::vector<int>>& vectors) {
-    return {};
+    std::vector<int64_t> scalars(words.size());
+    int64_t max = -LLONG_MAX;
+    int64_t max_count = 0;
+    for (size_t i = 1; i < words.size(); ++i) {
+        scalars[i] = Scalar(vectors[0], vectors[i]);
+        if(scalars[i] > max) {
+            max_count = 1;
+            max = scalars[i];
+        }
+        else if (scalars[i] == max) {
+            ++max_count;
+        }
+    }
+    std::vector<std::string> selected_words(max_count);
+    size_t index = 0;
+    for (size_t i = 0; i < scalars.size(); ++i) {
+        if (scalars[i] == max) {
+            selected_words[index++] = words[i];
+        }
+    }
+    return selected_words;
 }
