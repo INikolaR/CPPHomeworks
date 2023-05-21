2de4495fc8ee4867505069fc25c7e184d15e0397
= <=>
2023-05-21 18:52:18 +0300

    Finished

--- tasks/admission/admission.cpp
+++ tasks/admission/admission.cpp
@@ -2,7 +2,7 @@
 
 #include <tuple>
 
-const bool Comparator(const Applicant* first_applicant, const Applicant* second_applicant) {
+const bool Comparator(const Applicant*& first_applicant, const Applicant*& second_applicant) {
     if (first_applicant->points == second_applicant->points) {
         return std::tie(first_applicant->student.birth_date.year, first_applicant->student.birth_date.month,
                         first_applicant->student.birth_date.day, first_applicant->student.name) <
@@ -11,6 +11,13 @@ const bool Comparator(const Applicant* first_applicant, const Applicant* second_
     } else {
         return first_applicant->points > second_applicant->points;
     }
+}
+
+const bool StudentComparator(const Student* const& first_student, const Student* const& second_student) {
+    return std::tie(first_student->name, first_student->birth_date.year, first_student->birth_date.month,
+                    first_student->birth_date.day) < std::tie(second_student->name, second_student->birth_date.year,
+                                                              second_student->birth_date.month,
+                                                              second_student->birth_date.day);
 }
 
 AdmissionTable FillUniversities(const std::vector<University>& universities, const std::vector<Applicant>& applicants) {
@@ -28,7 +35,10 @@ AdmissionTable FillUniversities(const std::vector<University>& universities, con
     for (const Applicant* applicant : apps) {
         for (const std::string& name : applicant->wish_list) {
             if (admission_table[name].size() < limits[name]) {
-                admission_table[name].push_back(&applicant->student);
+                admission_table[name].insert(
+                    std::lower_bound(admission_table[name].begin(), admission_table[name].end(), &applicant->student,
+                                     StudentComparator),
+                    &applicant->student);
                 break;
             }
         }
