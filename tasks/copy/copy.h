436dd2182980eab245e0db165d956fe12eb9b440
= <=>
2023-05-24 20:48:08 +0300

    Finished

--- tasks/copy/copy.h
+++ tasks/copy/copy.h
@@ -1,7 +1,20 @@
 #pragma once
 
 template <typename T, typename U>
-U Copy(T src_begin, T src_end, U dest);
+U Copy(T src_begin, T src_end, U dest) {
+    for (U it = src_begin; it != src_end; ++it) {
+        *dest = *it;
+        ++dest;
+    }
+    return dest;
+}
 
 template <typename T, typename U>
-U CopyBackward(T src_begin, T src_end, U dest);
+U CopyBackward(T src_begin, T src_end, U dest) {
+    for (U it = src_end; it != src_begin;) {
+        --it;
+        --dest;
+        *dest = *it;
+    }
+    return dest;
+}
