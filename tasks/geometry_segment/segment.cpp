86d988d27a19c19d36900baf7c1f9ab8c4810c71
= <=>
2023-05-27 16:26:21 +0300

    Finished

--- tasks/geometry_segment/segment.cpp
+++ tasks/geometry_segment/segment.cpp
@@ -3,6 +3,18 @@
 #include "line.h"
 
 namespace geometry {
+bool lines_are_equal(Line a, Line b) {
+    if (a.GetC() < b.GetC()) {
+        std::swap(a, b);
+    }
+    // now a.GetC() >= b.GetC();
+    if (a.GetC() % b.GetC() != 0) {
+        return false;
+    }
+    int64_t d = a.GetC() / b.GetC();
+    return a.GetA() == b.GetA() * d && a.GetB() == b.GetB() * d;
+}
+
 Segment::Segment() {
     start_ = Point(0, 0);
     end_ = Point(0, 0);
@@ -55,10 +67,7 @@ bool Segment::CrossesSegment(const Segment& segment) const {
     }
     Line line_this = Line(start_, end_);
     Line line_that = Line(segment.start_, segment.end_);
-    if ((line_this.GetA() == line_that.GetA() && line_this.GetB() == line_that.GetB() &&
-         line_this.GetC() == line_that.GetC()) ||
-        (line_this.GetA() == -line_that.GetA() && line_this.GetB() == -line_that.GetB() &&
-         line_this.GetC() == -line_that.GetC())) {
+    if (lines_are_equal(line_this, line_that)) {
         return ContainsPoint(segment.GetStart()) || ContainsPoint(segment.GetEnd()) ||
                segment.ContainsPoint(GetStart()) || segment.ContainsPoint(GetEnd());
     }
