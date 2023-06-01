2844d2a5f3cf7373572c1edd3debd10eeb459e47
= <=>
2023-06-01 19:22:25 +0300

    Finished

--- tasks/vector/vector.h
+++ tasks/vector/vector.h
@@ -4,22 +4,22 @@ template <class T>
 class Vector {
 public:
     Vector() {
-        data_ = new int[1]{0};
+        data_ = new T[1]{0};
         size_ = 1;
         capacity_ = 1;
     }
 
     explicit Vector(size_t size) {
-        data_ = new int[size];
+        data_ = new T[size];
         size_ = size;
         capacity_ = size;
         for (size_t i = 0; i < size_; ++i) {
-            data_[i] = 0;
+            data_[i] = T();
         }
     }
 
     Vector(std::initializer_list<int> init_list) {
-        data_ = new int[init_list.size()];
+        data_ = new T[init_list.size()];
         size_ = 0;
         capacity_ = init_list.size();
         for (auto it = init_list.begin(); it != init_list.end(); ++it) {
@@ -27,12 +27,51 @@ public:
         }
     }
 
-    Vector(const &Vector copy) {
+    Vector(const Vector& copy) {
+        size_ = copy.size_;
+        capacity_ = copy.capacity_;
+        data_ = new T[capacity_];
+        for (size_t i = 0; i < size_; ++i) {
+            data_[i] = copy.data_[i];
+        }
+    }
 
+    Vector(Vector&& move) {
+        size_ = move.size_;
+        capacity_ = move.capacity_;
+        delete[] data_;
+        data_ = move.data_;
+    }
+
+    Vector& operator=(const Vector& copy) {
+        size_ = copy.size_;
+        capacity_ = copy.capacity_;
+        data_ = new T[capacity_];
+        for (size_t i = 0; i < size_; ++i) {
+            data_[i] = copy.data_[i];
+        }
+        return *this;
+    }
+
+    Vector& operator=(Vector&& move) {
+        size_ = move.size_;
+        capacity_ = move.capacity_;
+        delete[] data_;
+        data_ = move.data_;
+    }
+
+    ~Vector() {
+        delete[] data_;
+    }
+
+    void Swap(const Vector& vec1, const Vector& vec2) {
+        std::swap(vec1.data_, vec2.data_);
+        std::swap(vec1.size_, vec2.size_);
+        std::swap(vec1.capacity_, vec2.capacity_);
     }
 
 private:
-    int* data_;
+    T* data_;
     size_t size_;
     size_t capacity_;
 };
