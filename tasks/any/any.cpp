7167701aba2173948a70657f8205085c681ddf3c
= <=>
2023-06-10 12:21:17 +0300

    Finished

--- tasks/any/any.cpp
+++ tasks/any/any.cpp
@@ -1,1 +1,32 @@
 #include "any.h"
+
+#include <utility>
+
+Any::Any() {
+    ptr_ = nullptr;
+}
+
+Any &Any::operator=(Any rhs) {
+    Swap(rhs);
+    return *this;
+}
+
+Any::Any(const Any &rhs) : ptr_(rhs.ptr_->Clone()) {
+}
+
+Any::~Any() {
+    Clear();
+}
+
+bool Any::Empty() const {
+    return ptr_ == nullptr;
+}
+
+void Any::Clear() {
+    delete ptr_;
+    ptr_ = nullptr;
+}
+
+void Any::Swap(Any &rhs) {
+    std::swap(ptr_, rhs.ptr_);
+}
