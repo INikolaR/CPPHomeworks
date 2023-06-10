a9b0277ecb3f93b344a9df702b55999eb61694c2
= <=>
2023-06-10 15:01:09 +0300

    Finished

--- tasks/reversed/reversed.h
+++ tasks/reversed/reversed.h
@@ -17,3 +17,45 @@ public:
 private:
     Iterator begin_, end_;
 };
+
+template <class T, class U>
+class ReversedIterator {
+public:
+    ReversedIterator(T curr, U& value) : curr_(curr), value_(value) {
+    }
+
+    ReversedIterator& operator++() {
+        --curr_;
+        return *this;
+    }
+
+    U& operator*() {
+        return *curr_;
+    }
+
+    bool operator!=(const ReversedIterator& i) {
+        return curr_ != i.curr_;
+    }
+
+private:
+    T curr_;
+    U& value_;
+};
+
+class EmptyIterator {
+public:
+    EmptyIterator() {
+    }
+
+    bool operator!=(const EmptyIterator& i) {
+        return false;
+    }
+};
+
+template <class T>
+auto Reversed(T& c) {
+    if (c.empty()) {
+        return IteratorRange(EmptyIterator(), EmptyIterator());
+    }
+    return IteratorRange(ReversedIterator(--c.end(), *(--c.end())), ReversedIterator(--c.begin(), *(--c.end())));
+}
