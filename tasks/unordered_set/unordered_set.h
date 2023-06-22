035a3cae17bc0ef3c9c60ce464ec4e88d72ad2b9
= <=>
2023-06-22 18:37:11 +0300

    Finished

--- tasks/unordered_set/unordered_set.h
+++ tasks/unordered_set/unordered_set.h
@@ -8,8 +8,10 @@ class UnorderedSet {
 public:
     UnorderedSet() : n_elements_(0), data_() {
     }
+
     explicit UnorderedSet(size_t count) : n_elements_(0), data_(count) {
     }
+
     template <typename IteratorType,
               typename = std::enable_if_t<std::is_base_of_v<
                   std::forward_iterator_tag, typename std::iterator_traits<IteratorType>::iterator_category>>>
@@ -18,18 +20,22 @@ public:
             Insert(*i);
         }
     }
+
     UnorderedSet(const UnorderedSet& other) : n_elements_(other.n_elements_), data_(other.data_.size()) {
         for (size_t i = 0; i < data_.size(); ++i) {
             data_[i] = std::list<KeyT>(other.data_[i]);
         }
     }
+
     UnorderedSet(UnorderedSet&& other) {
         n_elements_ = std::move(other.n_elements_);
         data_ = std::move(other.data_);
         other.n_elements_ = 0;
         other.data_ = std::vector<std::list<KeyT>>(0);
     }
+
     ~UnorderedSet() = default;
+
     UnorderedSet& operator=(const UnorderedSet& other) {
         n_elements_ = other.n_elements_;
         data_ = std::vector<std::list<KeyT>>(other.data_.size());
@@ -38,6 +44,7 @@ public:
         }
         return *this;
     }
+
     UnorderedSet& operator=(UnorderedSet&& other) {
         n_elements_ = std::move(other.n_elements_);
         data_ = std::move(other.data_);
@@ -45,16 +52,20 @@ public:
         other.data_ = std::vector<std::list<KeyT>>(0);
         return *this;
     }
+
     size_t Size() const {
         return n_elements_;
     }
+
     bool Empty() const {
         return n_elements_ == 0;
     }
+
     void Clear() {
         data_.clear();
         n_elements_ = 0;
     }
+
     void Insert(const KeyT& key) {
         if (Find(key)) {
             return;
@@ -67,6 +78,7 @@ public:
         data_[std::hash<KeyT>{}(key) % BucketCount()].emplace_back(key);
         ++n_elements_;
     }
+
     void Insert(KeyT&& key) {
         if (Find(key)) {
             return;
@@ -79,6 +91,7 @@ public:
         data_[std::hash<KeyT>{}(key) % BucketCount()].emplace_back(std::move(key));
         ++n_elements_;
     }
+
     void Erase(const KeyT& key) {
         if (!Find(key)) {
             return;
@@ -92,6 +105,7 @@ public:
             }
         }
     }
+
     bool Find(const KeyT& key) const {
         if (Size() == 0) {
             return false;
@@ -104,6 +118,7 @@ public:
         }
         return false;
     }
+
     void Rehash(size_t new_bucket_count) {
         if (new_bucket_count == BucketCount()) {
             return;
@@ -120,6 +135,7 @@ public:
             }
         }
     }
+
     void Reserve(size_t new_bucket_count) {
         if (new_bucket_count <= BucketCount()) {
             return;
@@ -133,18 +149,22 @@ public:
             }
         }
     }
+
     size_t BucketCount() const {
         return data_.size();
     }
+
     size_t BucketSize(size_t id) const {
         if (id >= data_.size()) {
             return 0;
         }
         return data_[id].size();
     }
+
     size_t Bucket(const KeyT& key) const {
         return std::hash<KeyT>{}(key) % BucketCount();
     }
+
     double LoadFactor() const {
         if (BucketCount() == 0) {
             return 0;
